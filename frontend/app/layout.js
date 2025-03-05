acced44738c68d9b72da92caeff298a4a00bc2ba
Carlos Moneda <carlos@indava.com>
2025-03-04 19:24:49 -0600

    added fonts

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,8 +1,5 @@
 "use client"
-
-import { Geist, Geist_Mono } from "next/font/google";
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import "./globals.css";
 import Header from "@/app/components/Header/Header";
 import MainMenu from "@/app/components/MainMenu/MainMenu";
 import Box from "@mui/material/Box";
@@ -10,17 +7,23 @@ import {usePathname} from 'next/navigation'
 import {Container} from "@mui/material";
 import {SessionProvider} from "next-auth/react";
 import {AuthProvider} from "@/app/utils/contexts/AuthProvider";
-import { PrimeReactProvider } from 'primereact/api';
-import "primereact/resources/themes/lara-light-cyan/theme.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from "@/app/utils/theme";
+
+import { Inter, Rubik } from "next/font/google";
+const inter = Inter({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const rubik = Rubik({
+  weight: ['500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-rubik',
 });
 
 export default function RootLayout({ children }) {
@@ -29,11 +32,12 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-    <PrimeReactProvider>
       <AppRouterCacheProvider>
-        <SessionProvider>
-          <AuthProvider>
-            <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <SessionProvider>
+            <AuthProvider>
+              <body className={`${inter.variable} ${rubik.variable}`}>
               <Box sx={{
                 display: "grid",
                 gap: "20px",
@@ -50,14 +54,14 @@ export default function RootLayout({ children }) {
                   )
                 }
                 <Container  maxWidth="xl" component={"main"} sx={{gridColumn: noSidebarPages ? "1 / 3" : "2 / 3", paddingBottom: "20px", paddingRight: "20px", minHeight: "calc(100vh - 140px)"}}>
-                    {children}
+                  {children}
                 </Container>
               </Box>
-            </body>
-          </AuthProvider>
-        </SessionProvider>
+              </body>
+            </AuthProvider>
+          </SessionProvider>
+        </ThemeProvider>
       </AppRouterCacheProvider>
-    </PrimeReactProvider>
     </html>
   );
 }
