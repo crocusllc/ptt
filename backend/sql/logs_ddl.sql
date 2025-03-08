3929bd504511eb4d40e7e1a993cc150c8e49a481
Enrique Saucedo Fuentes <enrique@indava.com>
2025-03-08 17:49:43 -0600

    Delete constrait

--- backend/sql/logs_ddl.sql
+++ backend/sql/logs_ddl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE logs (
     log_id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(user_id) ON DELETE SET NULL,
-    file_name TEXT NOT NULL,
+    file_name TEXT NULL,
     action VARCHAR(50) CHECK (action IN ('uploaded', 'downloaded','created','updated','deleted','rejected')) NOT NULL,
     source_table VARCHAR(50) CHECK (source_table IN ('student_info', 'program_info', 'clinical_placements', 'all')) NOT NULL,
     timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
