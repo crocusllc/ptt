ef2f4e7f800d42d7ff32a803e272420cf136ab03
Enrique Saucedo Fuentes <enrique@indava.com>
2025-03-08 07:35:19 -0600

    Extended log table

--- backend/sql/logs_ddl.sql
+++ backend/sql/logs_ddl.sql
@@ -1,10 +1,14 @@
 CREATE TABLE logs (
     log_id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(user_id) ON DELETE SET NULL,
-    action VARCHAR(50) CHECK (action IN ('upload', 'download')) NOT NULL,
+    file_name TEXT NOT NULL,
+    action VARCHAR(50) CHECK (action IN ('uploaded', 'downloaded','created','updated','deleted','rejected')) NOT NULL,
     source_table VARCHAR(50) CHECK (source_table IN ('student_info', 'program_info', 'clinical_placements', 'all')) NOT NULL,
     timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    error_message TEXT NULL,
+    source_id INTEGER NULL,
     total_records INTEGER NOT NULL,
     valid_records INTEGER NOT NULL,
     invalid_records INTEGER NOT NULL
 );
+
