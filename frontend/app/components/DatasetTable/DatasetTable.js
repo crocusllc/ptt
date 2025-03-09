eda28300524b22444643c83f66d176c3585804ed
Carlos Moneda <carlos@indava.com>
2025-03-09 11:48:58 -0600

    added data table component

---
+++ frontend/app/components/DatasetTable/DatasetTable.js
@@ -0,0 +1,26 @@
+import {Column} from "primereact/column";
+import {DataTable} from "primereact/datatable";
+import React from "react";
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+
+export default function DatasetTable({rowsData, columnsData, selectionHook = null, selectionFn = null, selectionMode = null }) {
+
+  return(
+    <DataTable
+      value={rowsData}
+      showGridlines
+      paginator
+      rows={20}
+      rowsPerPageOptions={[5, 10, 20, 50]}
+      currentPageReportTemplate="{first} to {last} of {totalRecords}"
+      removableSort
+      selection={selectionHook}
+      onSelectionChange={selectionFn}
+      selectionMode={selectionMode}
+    >
+      { columnsData.map(col => (
+        <Column style={{fontSize: "14px"}} key={col.field} field={col.field} header={col.header} sortable={col.sortable} filter={col.filterEnabled} body={col.renderCell}/>
+      ))}
+    </DataTable>
+  )
+}
