44c8ca86bee65c15f15178824c295b71dcdcbe43
Carlos Moneda <carlos@indava.com>
2025-03-18 19:21:49 -0600

    updated page content

--- frontend/app/(pages)/forgot-password/page.js
+++ frontend/app/(pages)/forgot-password/page.js
@@ -1,79 +1,23 @@
 "use client"
-
 import Box from '@mui/material/Box';
 import {FormControl, FormLabel, TextField, Typography} from "@mui/material";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-
-import {useState} from "react";
 
 export default function ForgotPass() {
-  const [emailError, setEmailError] = useState(false);
-  const [emailErrorMessage, setEmailErrorMessage] = useState('');
-  const validateInputs = () => {
-    const email = document.getElementById('email');
-
-    let isValid = true;
-
-    if (!email.value || !/\S+@\S+\.\S+/.test(email?.value)) {
-      setEmailError(true);
-      setEmailErrorMessage('Please enter a valid email address.');
-      isValid = false;
-    } else {
-      setEmailError(false);
-      setEmailErrorMessage('');
-    }
-
-    return isValid;
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (emailError) {
-      return;
-    }
-    const data = new FormData(e.currentTarget);
-    //console.log(data);
-  };
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, alignItems: "center", justifyContent: "center"}}>
       <Card sx={{ maxWidth: 480, width: "80%" }}>
         <CardContent>
           <Typography gutterBottom component="h2" sx={{fontSize: "22px", marginBottom: "18px"}}>
-            Forgot Password
+            Forgot Password?
           </Typography>
           <Box
-            component="form"
-            onSubmit={handleSubmit}
+            component="div"
             sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
           >
-            <FormControl>
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <TextField
-                required
-                fullWidth
-                id="email"
-                placeholder="your@email.com"
-                name="email"
-                autoComplete="email"
-                variant="outlined"
-                error={emailError}
-                helperText={emailErrorMessage}
-                color={emailError ? 'error' : 'primary'}
-              />
-            </FormControl>
-            <Box sx={{display: "flex", justifyContent: "flex-end"}}>
-              <Button
-                sx={{maxWidth: "200px"}}
-                type="submit"
-                fullWidth
-                variant="contained"
-                onClick={validateInputs}
-              >
-                Send
-              </Button>
-            </Box>
+            Please contact your administrator.
           </Box>
         </CardContent>
       </Card>
